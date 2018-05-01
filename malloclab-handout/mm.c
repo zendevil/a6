d0f04302cc53834358e359032dbedcc28b047989
Prikshet <pshar10@u.rochester.edu>
2018-05-01 19:44:00 -0400

    free done.

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -35,6 +35,23 @@ team_t team = {
     "nmccloud@u.rochester.edu"
 };
 
+
+#define WSIZE 4
+#define DSIZE 8
+//given block pointer, compute address of its header and footer.
+#define HDRP(bp)    ((char*)(bp) - WSIZE)
+
+#define FTRP(bp)            ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define PUT(p, val) 
+#define GET(p) (*(unsigned int *) (p))
+#define GET_SIZE(p) (GET(p) && ~0x7)
+
+#define GET_ALLOC(p)        (GET(p) & 0x1)
+#define PREV_BLKP(bp)       ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+#define NEXT_BLKP(bp)       ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
@@ -68,16 +85,62 @@ void *mm_malloc(size_t size)
     }
 }
 
+static int aligned(const void* p) {
+    return ((unsigned long)p % ALIGNMENT) == 0;
+}
+
+static int in_heap(const void* p) {
+    return p <= mem_heap_hi() && p >= mem_heap_lo();
+}
+
+static void *coalesce(void * bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc)
+        return bp;
+
+    else if (prev_alloc && !next_alloc)
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+    else if (!prev_alloc && next_alloc)
+    {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    else
+    {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+
 /*
  * mm_free - Freeing a block does nothing.
  */
 void mm_free(void *ptr)
 {
-    while(ptr) {
-        ptr = 0;
-        ptr++;
-    }
+    size_t size = GET_SIZE(HDRP(ptr));
+
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+    coalesce(ptr);
+
 }
+
+
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
