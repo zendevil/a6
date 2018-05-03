1920cfa54837c430712eda6205dbe17378a9576d
Prikshet <pshar10@u.rochester.edu>
2018-05-03 13:55:38 -0400

    test

--- malloclab-handout/mm.h
+++ malloclab-handout/mm.h
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-extern int mm_init (void);
-extern void *mm_malloc (size_t size);
-extern void mm_free (void *ptr);
-extern void *mm_realloc(void *ptr, size_t size);
+extern static int mm_init (void);
+extern static void *mm_malloc (size_t size);
+extern static void mm_free (void *ptr);
+extern static void *mm_realloc(void *ptr, size_t size);
 
 
 /* 
